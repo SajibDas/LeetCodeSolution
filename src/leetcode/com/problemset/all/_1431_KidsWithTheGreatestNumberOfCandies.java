500a773c16e6d8e15fe3efd3f72708d6b156e4db
Sajib Das <ece100930@gmail.com>
2020-05-26 20:05:57 +0900

    Runtime: 0 ms, faster than 100.00% Memory Usage: 39.5 MB, less than 100.00%

---
+++ src/leetcode/com/problemset/all/_1431_KidsWithTheGreatestNumberOfCandies.java
@@ -0,0 +1,29 @@
+package leetcode.com.problemset.all;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class _1431_KidsWithTheGreatestNumberOfCandies {
+    
+	/**
+	 * Problem : https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
+	 * @param candies
+	 * @param extraCandies
+	 * @return
+	 */
+	public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int max = candies[0];
+        for(int i : candies){
+            max = max < i ? i : max;
+        }
+        List<Boolean> kidsList = new ArrayList<Boolean>();
+        for(int j : candies){
+            if(max <= j+extraCandies){
+                kidsList.add(true);
+            }else{
+                kidsList.add(false);
+            }
+        }
+        return kidsList;
+    }
+}
